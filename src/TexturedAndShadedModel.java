bd0c736d2b316cb2d8dd4434f3c58e272b171ebd
Folkol <mattias4@kth.se>
2013-06-16 16:52:21 +0200

    Now culling back faces

--- src/TexturedAndShadedModel.java
+++ src/TexturedAndShadedModel.java
@@ -50,6 +50,7 @@ public class TexturedAndShadedModel extends Base {
 
     @Override
     void renderScene() {
+        GL11.glEnable(GL11.GL_CULL_FACE);
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 
         GL20.glUseProgram(pId);
